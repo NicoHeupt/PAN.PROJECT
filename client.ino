904e7c7d132e68fefbe384a6b2cc01ccbc661e3f
Paul Seibel <paul.seibel@mailbox.org>
2018-12-14 16:47:49 +0100

    more experiements, somewaht working client

--- client.ino
+++ client.ino
@@ -1,103 +1,51 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
  
-// WiFi information
-const char WIFI_SSID[] = "Wanderhure";
-const char WIFI_PSK[] = "3kreuzer";
+const char* ssid = "Wanderhure";
+const char* password = "3kreuzer";
+const int ledPin = 4;
+
+int ledBrightness;
+
+// calculate the value to put out of the analog pin
+// change this function to your needs
+byte calcBrightness(byte sensorReading) {
+  return sensorReading;
+}
  
-// Remote site information
-const char http_site[] = "192.168.8.163/sensor";
-const int http_port = 80;
+void setup () {
  
-// Pin definitions
-const int LED_PIN = 4;
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting..");
  
-// Global variables
-WiFiClient client;
- 
-void setup() {
-  
-  // Set up serial console to read web page
-  Serial.begin(9600);
-  Serial.print("Thing GET Example");
-  
-  // Set up LED for debugging
-  pinMode(LED_PIN, OUTPUT);
-  
-  // Connect to WiFi
-  connectWiFi();
-  
-  // Attempt to connect to website
-  if ( !getPage() ) {
-    Serial.println("GET request failed");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300); 
   }
+ 
 }
  
 void loop() {
-  
-  // If there are incoming bytes, print them
-  if ( client.available() ) {
-    char c = client.read();
-    Serial.print(c);
-  }
-  
-  // If the server has disconnected, stop the client and WiFi
-  if ( !client.connected() ) {
-    Serial.println();
-    
-    // Close socket and wait for disconnect from WiFi
-    client.stop();
-    if ( WiFi.status() != WL_DISCONNECTED ) {
-      WiFi.disconnect();
-    }
-    
-    // Turn off LED
-    digitalWrite(LED_PIN, LOW);
-    
-    // Do nothing
-    Serial.println("Finished Thing GET test");
-    while(true){
-      delay(1000);
-    }
-  }
-}
  
-// Attempt to connect to WiFi
-void connectWiFi() {
-  
-  byte led_status = 0;
-  
-  // Set WiFi mode to station (client)
-  WiFi.mode(WIFI_STA);
-  
-  // Initiate connection with SSID and PSK
-  WiFi.begin(WIFI_SSID, WIFI_PSK);
-  
-  // Blink LED while we wait for WiFi connection
-  while ( WiFi.status() != WL_CONNECTED ) {
-    digitalWrite(LED_PIN, led_status);
-    led_status ^= 0x01;
-    delay(100);
-  }
-  
-  // Turn LED on when we are connected
-  digitalWrite(LED_PIN, HIGH);
-}
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
  
-// Perform an HTTP GET request to a remote page
-bool getPage() {
-  
-  // Attempt to make a connection to the remote server
-  if ( !client.connect(http_site, http_port) ) {
-    return false;
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://192.168.8.163/sensor");  //Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+      String payload = http.getString();   //Get the request response payload
+      //ledBrightness = payload.ToInt();
+      Serial.println(payload);                     //Print the response payload
+      //analogWrite(ledPin, ledBrightness);
+    }
+    http.end();   //Close connection
+ 
   }
-  
-  // Make an HTTP GET request
-  client.println("GET /index.html HTTP/1.1");
-  client.print("Host: ");
-  client.println(http_site);
-  client.println("Connection: close");
-  client.println();
-  
-  return true;
+  delay(1000);    //Send a request every second
+ 
 }
